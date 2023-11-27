728f3e8517d9cf9bde45f54bcdd9a2ead19aadbc
Ogie Eweka <oieconsulting23@gmail.com>
2023-11-27 01:56:18 -0500

    added password to linux vm and saved to key vault

--- spoke1/virtualMachines/testvm/main.tf
+++ spoke1/virtualMachines/testvm/main.tf
@@ -1,6 +1,8 @@
 locals {
   name        = "oie-eus-sandbox-test-linux-vm"
   subnet_data = "oie-eus-sandbox"
+  kv_name         = "oie-sandbox-key-vault"
+  kv_rg       = "oie-sandbox-key-vault"
 }
 
 # RESOURCE GROUP
@@ -9,11 +11,17 @@ resource "azurerm_resource_group" "vm_rg" {
   location = var.location
 }
 
-# SPOKE 1 FRONT END SUBNET DATA SOURCE
+# SPOKE FRONT END SUBNET DATA SOURCE
 data "azurerm_subnet" "web_subnet" {
   name                 = "${local.subnet_data}-web-subnet"
   virtual_network_name = "${local.subnet_data}-vnet"
   resource_group_name  = "${local.subnet_data}-rg"
+}
+
+# KEY VAULT
+data "azurerm_key_vault" "this" {
+  name                = local.kv_name
+  resource_group_name = "${local.kv_rg}-rg"
 }
 
 # NIC
@@ -34,14 +42,30 @@ resource "random_pet" "this" {
   length = 1
 }
 
-# HUB VIRTUAL MACHINE
+# GENERATE VM PASSWORD
+resource "random_password" "admin_password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+# SAVE PASSWORD TO KEYVAULT
+resource "azurerm_key_vault_secret" "vm_password" {
+  name         = "${local.name}-admin-password"
+  value        = random_password.admin_password.result
+  key_vault_id = data.azurerm_key_vault.this.id
+}
+
+# LINUX VIRTUAL MACHINE
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                  = local.name
-  location              = azurerm_resource_group.vm_rg.location
-  resource_group_name   = azurerm_resource_group.vm_rg.name
-  network_interface_ids = [azurerm_network_interface.vm_nic.id]
-  size                  = var.vmsize
-  admin_username        = var.username
+  name                            = local.name
+  location                        = azurerm_resource_group.vm_rg.location
+  resource_group_name             = azurerm_resource_group.vm_rg.name
+  network_interface_ids           = [azurerm_network_interface.vm_nic.id]
+  size                            = var.vmsize
+  admin_username                  = var.username
+  admin_password                  = random_password.admin_password.result
+  disable_password_authentication = false
 
   admin_ssh_key {
     username   = var.username
