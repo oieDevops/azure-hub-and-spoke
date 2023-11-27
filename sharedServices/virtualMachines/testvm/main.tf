996d5a30984020d334b3f8d1c08b81528a38b58a
Ogie Eweka <oieconsulting23@gmail.com>
2023-11-27 02:11:30 -0500

    added password to linux vm and saved to key vault

--- sharedServices/virtualMachines/testvm/main.tf
+++ sharedServices/virtualMachines/testvm/main.tf
@@ -1,6 +1,8 @@
 locals {
   name        = "oie-eus-sharedservices-test-linux-vm"
   subnet_data = "oie-eus-sharedservices"
+  kv_name     = "oie-sandbox-key-vault"
+  kv_rg       = "oie-sandbox-key-vault"
 }
 
 # RESOURCE GROUP
@@ -14,6 +16,12 @@ data "azurerm_subnet" "mgmt_subnet" {
   name                 = "${local.subnet_data}-mgmt-subnet"
   virtual_network_name = "${local.subnet_data}-vnet"
   resource_group_name  = "${local.subnet_data}-rg"
+}
+
+# KEY VAULT
+data "azurerm_key_vault" "this" {
+  name                = local.kv_name
+  resource_group_name = "${local.kv_rg}-rg"
 }
 
 # NIC
@@ -34,6 +42,20 @@ resource "random_pet" "this" {
   length = 1
 }
 
+# GENERATE VM PASSWORD
+resource "random_password" "admin_password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+# SAVE PASSWORD TO KEYVAULT
+resource "azurerm_key_vault_secret" "vm_password" {
+  name         = "${local.name}-admin-password"
+  value        = random_password.admin_password.result
+  key_vault_id = data.azurerm_key_vault.this.id
+}
+
 # HUB VIRTUAL MACHINE
 resource "azurerm_linux_virtual_machine" "vm" {
   name                  = local.name
@@ -42,11 +64,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
   network_interface_ids = [azurerm_network_interface.vm_nic.id]
   size                  = var.vmsize
   admin_username        = var.username
-
-  admin_ssh_key {
-    username   = var.username
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
+  admin_password                  = random_password.admin_password.result
+  disable_password_authentication = false
 
   os_disk {
     name                 = "${local.name}-osdisk1-${random_pet.this.id}"
